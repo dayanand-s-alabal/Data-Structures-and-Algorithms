06632b527cf598adf9a4c6fbd5a1b2d647f3178a
Dayanand S Alabal <dayanandalabal@gmail.com>
2026-08-27 19:29:31 +0530

    Time: 48 ms (8.63%) | Memory: 46.9 MB (74.53%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -6,6 +6,7 @@ class Solution {
                 if(nums[i] + nums[j] == target){
                     ret[0] = i;
                     ret[1] = j;
+                    return ret;
                 }
             }
         }
