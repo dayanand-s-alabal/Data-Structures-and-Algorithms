820ae180ec5de5f0c6e37e205d04c9c21b0db8d5
Dayanand S Alabal <dayanandalabal@gmail.com>
2026-09-02 22:30:31 +0530

    Time: 3 ms (100.00%) | Memory: 175.3 MB (70.57%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,18 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positive = new ArrayList<>(nums.length/2);
-        List<Integer> negative = new ArrayList<>(nums.length/2);
-        for(int num : nums){
-            if(num < 0){
-                negative.add(num);
+
+        //BRUTEFORCE APPROACH
+        // List<Integer> positive = new ArrayList<>(nums.length/2);
+        // List<Integer> negative = new ArrayList<>(nums.length/2);
+        // for(int num : nums){
+        //     if(num < 0){
+        //         negative.add(num);
+        //     }else{
+        //         positive.add(num);
+        //     }
+        // }
+        // for(int i=0;i<nums.length/2;i++){
+        //     nums[2*i] = positive.get(i);
+        //     nums[2*i + 1] = negative.get(i);
+        // }
+        // return nums;
+
+
+        // OPTIMAL APPROACH
+        int pos = 0,neg = 1;
+        int count = 0;
+        int arr[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > 0){
+                arr[pos] = nums[i];
+                pos += 2;
             }else{
-                positive.add(num);
+                arr[neg] = nums[i];
+                neg += 2; 
             }
         }
-        for(int i=0;i<nums.length/2;i++){
-            nums[2*i] = positive.get(i);
-            nums[2*i + 1] = negative.get(i);
-        }
-        return nums;
+        return arr;
     }
 }
